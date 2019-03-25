b8ab9ae07656ec5510ca8c2b0664fafcfcdd69fe
Anthony Caligure <acaligu2@slic-40.pods.bu.int>
2019-03-25 13:42:50 -0400

    Removed default Spritekit demo-code

--- Juggling Simulator/GameScene.swift
+++ Juggling Simulator/GameScene.swift
@@ -11,9 +11,6 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
     override func didMove(to view: SKView) {}
     
     
