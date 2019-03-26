70642d857dfeebc85ec95fb0ed137b4ff7c7ae49
Bradley Choy <bchoy3@slic-37.pods.bu.int>
2019-03-26 14:56:47 -0400

    Conciatori and I worked on the game logic and juggling physics

--- Juggling Simulator/GameScene.swift
+++ Juggling Simulator/GameScene.swift
@@ -6,6 +6,9 @@
 //  Copyright © 2019 CaligureConciatoriChoy. All rights reserved.
 //
 
+/*
+For catch change position to glove position and stop moving the ball
+*/
 import SpriteKit
 import GameplayKit
 
@@ -13,12 +16,12 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    let ball1 = SKSpriteNode(imageNamed:"ball1")
-    let ball2 = SKSpriteNode(imageNamed:"ball2")
-    let ball3 = SKSpriteNode(imageNamed:"ball3")
+    let greenBall = SKSpriteNode(imageNamed:"ball1")
+    let redBall = SKSpriteNode(imageNamed:"ball2")
+    let yellowBall = SKSpriteNode(imageNamed:"ball3")
     
-    let glove1 = SKSpriteNode(imageNamed:"leftGlove")
-    let glove2 = SKSpriteNode(imageNamed:"rightGlove")
+    let leftGlove = SKSpriteNode(imageNamed:"leftGlove")
+    let rightGlove = SKSpriteNode(imageNamed:"rightGlove")
     
     lazy var gameTitle: SKLabelNode = {
         var label = SKLabelNode()
@@ -52,43 +55,140 @@ class GameScene: SKScene {
         
         //Create gloves and set position
         
-        glove1.setScale(0.1)
-        glove1.position = CGPoint(x: self.size.width * 0.25, y: self.size.height * 0.2)
-        glove1.zPosition = 1
+        leftGlove.setScale(0.15)
+        leftGlove.position = CGPoint(x: self.size.width * 0.25, y: self.size.height * 0.2)
+        leftGlove.zPosition = 1
         
-        self.addChild(glove1)
+        self.addChild(leftGlove)
         
-        glove2.setScale(0.1)
-        glove2.position = CGPoint(x: self.size.width * 0.75, y: self.size.height * 0.2)
-        glove2.zPosition = 1
+        rightGlove.setScale(0.15)
+        rightGlove.position = CGPoint(x: self.size.width * 0.75, y: self.size.height * 0.2)
+        rightGlove.zPosition = 1
         
-        self.addChild(glove2)
+        self.addChild(rightGlove)
         
         //Create balls and set position
         
-        ball1.setScale(0.125)
-        ball1.position = CGPoint(x: self.size.width * 0.5, y: self.size.height * 0.75)
-        ball1.zPosition = 2
+        //red
+        greenBall.setScale(0.125)
+        greenBall.position = CGPoint(x: self.size.width * 0.21, y: self.size.height * 0.2)
+        greenBall.zPosition = 2
         
-        self.addChild(ball1)
+        self.addChild(greenBall)
         
-        ball2.setScale(0.3)
-        ball2.position = CGPoint(x: self.size.width * 0.5, y: self.size.height * 0.5)
-        ball2.zPosition = 2
         
-        self.addChild(ball2)
+        redBall.setScale(0.3)
+        redBall.position = CGPoint(x: self.size.width * 0.25, y: self.size.height * 0.2)
+        redBall.zPosition = 3
         
-        ball3.setScale(0.25)
-        ball3.position = CGPoint(x: self.size.width * 0.5, y: self.size.height * 0.25)
-        ball3.zPosition = 2
+        self.addChild(redBall)
         
-        self.addChild(ball3)
+        yellowBall.setScale(0.25)
+        yellowBall.position = CGPoint(x: self.size.width * 0.76, y: self.size.height * 0.2)
+        yellowBall.zPosition = 4
+        
+        self.addChild(yellowBall)
         
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //right seqence moves the ball from the left hand to the right hand
+        let moveRight = SKAction.moveTo(x: self.frame.size.width/2, duration: 5.0)
+        let moveUp = SKAction.moveTo(y: self.frame.size.height, duration: 5.0)
+        let moveDown = SKAction.moveTo(y: rightGlove.position.y, duration: 5.0)
+        let moveRighter = SKAction.moveTo(x: rightGlove.position.x, duration: 5.0)
+        let group1 = SKAction.group([moveRight,moveUp])
+        let group2 = SKAction.group([moveDown,moveRighter])
+        let sequenceRight = SKAction.sequence([group1,group2])
+
         
+        //left sequence moves the ball from the right hand to the left hand
+        let moveLeft = SKAction.moveTo(x: self.frame.size.width/2, duration: 5.0)
+        let moveDownLeft = SKAction.moveTo(y: leftGlove.position.y, duration: 5.0)
+        let moveLefter = SKAction.moveTo(x: leftGlove.position.x, duration: 5.0)
+
+        let group3 = SKAction.group([moveLeft,moveUp])
+        let group4 = SKAction.group([moveDownLeft,moveLefter])
+        let sequenceLeft = SKAction.sequence([group3,group4])
+        
+        
+        //redBall.run(sequenceRight)//red ball in left hand, throw right
+        //redBall.run(sequenceLeft)//red ball in right hand, throw left
+        //yellowBall.run(sequenceLeft)//yellow ball in right hand, throw left
+        //yellowBall.run(sequenceRight)//yellow ball in left hand, throw right
+        greenBall.run(sequenceRight)//green ball in left hand, throw right
+        //greenBall.run(sequenceLeft)//green ball in right hand, throw left
+    }
+
+    
+    override func update(_ currentTime:TimeInterval){
+        
+        let leftSize = leftGlove.size
+        let rightSize = leftGlove.size
+        
+        if(greenBall.position.x < leftGlove.position.x + leftSize.width && greenBall.position.x > leftGlove.position.x - leftSize.width) {
+            
+            if(greenBall.position.y < leftGlove.position.y + leftSize.height && greenBall.position.y > leftGlove.position.y - leftSize.height) {
+                
+                //do catch
+            }
+        }
+        
+        else if(greenBall.position.x < rightGlove.position.x + rightSize.width && greenBall.position.x > rightGlove.position.x - rightSize.width) {
+            
+            if(greenBall.position.y < rightGlove.position.y + rightSize.height && greenBall.position.y > rightGlove.position.y - rightSize.height) {
+                
+                //do catch
+            }
+        }
+        
+        else {
+            
+            
+        }
+        
+        if(yellowBall.position.x < leftGlove.position.x + leftSize.width && yellowBall.position.x > leftGlove.position.x - leftSize.width) {
+            
+            if(yellowBall.position.y < leftGlove.position.y + leftSize.height && yellowBall.position.y > leftGlove.position.y - leftSize.height) {
+                
+                //do catch
+            }
+        }
+            
+        else if(yellowBall.position.x < rightGlove.position.x + rightSize.width && yellowBall.position.x > rightGlove.position.x - rightSize.width) {
+            
+            if(yellowBall.position.y < rightGlove.position.y + rightSize.height && yellowBall.position.y > rightGlove.position.y - rightSize.height) {
+                
+                //do catch
+            }
+        }
+            
+        else {
+            
+            
+        }
+        
+        if(redBall.position.x < leftGlove.position.x + leftSize.width && redBall.position.x > leftGlove.position.x - leftSize.width) {
+            
+            if(redBall.position.y < leftGlove.position.y + leftSize.height && redBall.position.y > leftGlove.position.y - leftSize.height) {
+                
+                //do catch
+            }
+        }
+            
+        else if(redBall.position.x < rightGlove.position.x + rightSize.width && redBall.position.x > rightGlove.position.x - rightSize.width) {
+            
+            if(redBall.position.y < rightGlove.position.y + rightSize.height && redBall.position.y > rightGlove.position.y - rightSize.height) {
+                
+                //do catch
+            }
+        }
+            
+        else {
+            
+            
+        }
     }
     
-}
+}//ends program
